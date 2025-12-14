e85a0ae6fdb40ef37a66ee3c5531e31449330f9e
VaibhaviUN1999 <vaibhaviun.xworkz@gmail.com>
2025-12-14 14:43:50 +0530

    new

---
+++ append.py
@@ -0,0 +1,5 @@
+file=open("example.txt",'a')
+
+file.write("Lets start with file operations")
+
+file.close()
---
+++ write.py
@@ -0,0 +1,6 @@
+file=open("example.txt",'w')
+
+file.write("Hi Students\n")
+file.write("Welcome to File Handling")
+
+file.close()
